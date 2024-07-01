482bb157b526a375717989b129276c5f1e49b7a4
Nikolay Nechaev <nikolay_nechaev@mail.ru>
2024-07-01 19:09:56 +0300

    Implemented stub chrdev driver

--- ring-chrdev.c
+++ ring-chrdev.c
@@ -1,18 +1,78 @@
 #include <linux/module.h>
 #include <linux/printk.h>
+#include <linux/fs.h>
+
+static int ring_open(struct inode *inode, struct file *filp) {
+#ifdef DEBUG
+    pr_debug("ring_open: major=%u, minor=%u\n", imajor(inode), iminor(inode));
+#endif
+
+   return 0;
+}
+
+static int ring_release(struct inode *inode, struct file *filp) {
+#ifdef DEBUG
+    pr_debug("ring_release: major=%u, minor=%u\n", imajor(inode), iminor(inode));
+#endif
+
+    return 0;
+}
+
+static ssize_t ring_read(struct file *filp, char __user *buffer,
+                         size_t length, loff_t *offset)
+{
+#ifdef DEBUG
+    pr_debug("ring_read: offset=%lld, len=%ld\n", *offset, length);
+#endif
+
+    // Nothing to be read
+    return 0;
+}
+
+static ssize_t ring_write(struct file *filp, const char __user *buff,
+                          size_t length, loff_t *offset)
+{
+#ifdef DEBUG
+    pr_debug("ring_write: offset=%lld, len=%ld\n", *offset, length);
+#endif
+
+    // Operation not supported
+    return -ENOSYS;
+}
+
+
+static const struct file_operations chardev_fops = {
+    // Make sure this module isn't unloaded while a file is open.
+    .owner = THIS_MODULE,
+
+    .read = ring_read,
+    .write = ring_write,
+    .open = ring_open,
+    .release = ring_release,
+};
+
+
+#define RING_CHRDEV_NAME "ring"
+static int major;
 
 static int __init ring_init(void) {
-#ifdef DEBUG
-    pr_debug("Initializing ring-chrdev\n");
-#endif
+    major = register_chrdev(0, RING_CHRDEV_NAME, &chardev_fops);
+    if (major < 0) {
+        pr_err("ring-chrdev: failed to `register_chrdev`: %d\n", major);
+        return major;
+    }
+
+    pr_info("ring-chrdev: registered with major=%d\n", major);
 
     return 0;
 }
 
 static void __exit ring_cleanup(void) {
 #ifdef DEBUG
-    pr_debug("Cleaning up ring-chrdev\n");
+    pr_debug("ring-chrdev: cleaning up\n");
 #endif
+
+    unregister_chrdev(major, RING_CHRDEV_NAME);
 }
 
 module_init(ring_init);
